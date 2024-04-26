c53caaca5aa1738a36046199c5d76fb5ceeed9f3
Nguyen Thanh Hy <job.bkth@gmail.com>
2024-04-27 02:35:28 +0700

    Commit message

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -73,6 +73,20 @@ async function fetchData() {
       return filteredData;
     }
 
+    function getDataFromServer() {
+      $.ajax({
+        method: "GET",
+        url: "/data", // Thay thế đường dẫn với URL của dữ liệu JSON trên máy chủ của bạn
+        dataType: "json",
+        success: function (response) {
+          // Xử lý dữ liệu JSON được nhận từ máy chủ ở đây
+          console.log("Data from server:", response);
+          return response;
+        },
+        error: function (xhr, status, error) {
+          console.error("Error fetching data:", error);
+        },
+      });
     }
 
     // Build Table function
@@ -96,146 +110,130 @@ async function fetchData() {
                             <button class="btn btn-sm btn-success" data-id=${data.ID} id="cancel-${data.ID}">Cancel</button>
                         </div>
                     </td>
+                </tr>`;
+      $("#myTable").append(row);
 
-            </tr>`
-            $('#myTable').append(row)
+      $(`#delete-${data.ID}`).on("click", deleteData);
+      $(`#cancel-${data.ID}`).on("click", cancelDeletion);
+      $(`#confirm-${data.ID}`).on("click", confirmDeletion);
+      $(`#change-${data.ID}`).on("click", editData);
+    }
 
-            $(`#delete-${data.ID}`).on('click', deleteData)
-            $(`#cancel-${data.ID}`).on('click', cancelDeletion)
-            $(`#confirm-${data.ID}`).on('click', confirmDeletion)
-            $(`#change-${data.ID}`).on('click', editData)
-        }
-
-        function buildTable(data){
-            console.log("build table called")
-            var table = document.getElementById('myTable')
-            table.innerHTML = ''
-            for (var i in data){
-                addRow(data[i])
-            }
-        }
-        buildTable(test)
+    function buildTable(data) {
+      console.log("build table called");
+      var table = document.getElementById("myTable");
+      table.innerHTML = "";
+      for (var i in data) {
+        addRow(data[i]);
+      }
+    }
+    buildTable(test);
 
     // Delete data fucntion
-        function deleteData(){
-            // console.log("deldata called")
-            var dataID = $(this).data('id')
-            var change_del_Btn = $(`#change-del-btn-${dataID}`)
-            var cancel_confirm_Btn = $(`#cancel-confirm-btn-${dataID}`)
+    function deleteData() {
+      // console.log("deldata called")
+      var dataID = $(this).data("id");
+      var change_del_Btn = $(`#change-del-btn-${dataID}`);
+      var cancel_confirm_Btn = $(`#cancel-confirm-btn-${dataID}`);
 
-            change_del_Btn.addClass('hidden')
-            change_del_Btn.removeClass('show')
-            cancel_confirm_Btn.addClass('show')
-            cancel_confirm_Btn.removeClass('hidden')
+      change_del_Btn.addClass("hidden");
+      change_del_Btn.removeClass("show");
+      cancel_confirm_Btn.addClass("show");
+      cancel_confirm_Btn.removeClass("hidden");
+    }
+    function cancelDeletion() {
+      var dataID = $(this).data("id");
+      var change_del_Btn = $(`#change-del-btn-${dataID}`);
+      var cancel_confirm_Btn = $(`#cancel-confirm-btn-${dataID}`);
 
+      change_del_Btn.addClass("show");
+      change_del_Btn.removeClass("hidden");
+      cancel_confirm_Btn.addClass("hidden");
+      cancel_confirm_Btn.removeClass("show");
+    }
+    function confirmDeletion() {
+      var dataID = $(this).data("id");
+      var row = $(`.data-row-${dataID}`);
+      row.remove();
 
-        };
-        function cancelDeletion(){
-            var dataID = $(this).data('id')
-            var change_del_Btn = $(`#change-del-btn-${dataID}`)
-            var cancel_confirm_Btn = $(`#cancel-confirm-btn-${dataID}`)
+      // handle Backend database
+      // after actually delete the data in database
+      // $.ajax({
+      //     method: 'GET',
+      //     url: , //url send data
+      //     dataType: 'json' ,
+      //     success:function(response){
+      //         data = response.data
+      //         buildTable(data)
+      //         // console.log(data)
+      //     }
+      // })
+      popupDialog("Success", "Data deleted");
+    }
 
-            change_del_Btn.addClass('show')
-            change_del_Btn.removeClass('hidden')
-            cancel_confirm_Btn.addClass('hidden')
-            cancel_confirm_Btn.removeClass('show')
-        };
-        function confirmDeletion(){
-            var dataID = $(this).data('id');
+    // Edit data
+    function editData() {
+      var dataID = $(this).data("id");
+      console.log(dataID);
+      $("#formChange").addClass("show");
+      $("#dataTable").css({
+        opacity: "0.5",
+        "pointer-events": "none",
+      });
 
-            var data = getDatasInRow(dataID);
+      // Get old data
+      $("#changeName").val($(`#data-name-${dataID}`).text());
+      $("#changePhone").val($(`#data-phone-${dataID}`).text());
+      $("#changeEmail").val($(`#data-email-${dataID}`).text());
+      $("#changeAdress").val($(`#data-address-${dataID}`).text());
+      $("#changeVeID").val($(`#data-license-${dataID}`).text());
+      $("#changeBrand").val($(`#data-brand-${dataID}`).text());
+      $("#changeDate").val($(`#data-date-${dataID}`).text());
+      // submit event
+      console.log("before sub", test);
+      $("#formChange form")
+        .data("id", dataID)
+        .off("submit")
+        .on("submit", function (event) {
+          console.log("dataID sub", dataID);
+          // var datasubID = $(this).data('id')
+          // console.log("submitted")
+          event.preventDefault();
+          var updatedData = {
+            ID: dataID,
+            Name: $("#changeName").val(),
+            Phone: $("#changePhone").val(),
+            Email: $("#changeEmail").val(),
+            Address: $("#changeAdress").val(),
+            "Vehicle license number": $("#changeVeID").val(),
+            "Vehicle brand": $("#changeBrand").val(),
+            Date: $("#changeDate").val(),
+          };
+          console.log("updated data: ", updatedData);
+          updateData(dataID, updatedData);
 
-            $.ajax({
-                url: '/delete-row-form1',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify(data),
-                success: function(response) {
-                    // alert('Data sent successfully:');
-                },
-                error: function(xhr, status, error) {
-                    alert(status + ': ' + error);
-                }
-            });
+          $("#formChange").addClass("hidden");
+          $("#formChange").removeClass("show");
+          $("body").css("overflow", "auto");
+          $("#dataTable").css({
+            opacity: "",
+            "pointer-events": "",
+          });
+        });
+      // Handle updatedData to database
 
-            var row = $(`.data-row-${dataID}`);
-            row.remove();
-
-            popupDialog("Success", "Data deleted")
-
-        };
-
-
-        function getDatasInRow(dataID){
-            var phoneNumber = document.getElementById("data-phone-" + dataID.toString()).textContent;
-            var name = document.getElementById("data-name-" + dataID.toString()).textContent;
-            var email = document.getElementById("data-email-" + dataID.toString()).textContent;
-            var address = document.getElementById("data-address-" + dataID.toString()).textContent;
-            var license = document.getElementById("data-license-" + dataID.toString()).textContent;
-            var brand = document.getElementById("data-brand-" + dataID.toString()).textContent;
-            var date = document.getElementById("data-date-" + dataID.toString()).textContent;
-
-            return  data = {
-                recordID: dataID,
-                name: name,
-                phone: phoneNumber,
-                email: email,
-                address: address,
-                license: license,
-                brand: brand,
-                date: date
-            };
-        }
-
-// Edit data
-        function editData(){
-            var dataID = $(this).data('id')
-            console.log(dataID)
-            $('#formChange').addClass('show')
-            $('#dataTable').css({
-                'opacity': '0.5',
-                'pointer-events': 'none'
-            });
-
-            var data = getDatasInRow(dataID);
-
-            $.ajax({
-                url: '/get-old-data-change-form1',
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify(data),
-                success: function(response) {
-                    // alert("abc")
-                },
-                error: function(xhr, status, error) {
-                    alert(status + ': ' + error);
-                }
-            });
-
-            // Get old data
-            $("#changeName").val($(`#data-name-${dataID}`).text());
-            $("#changePhone").val($(`#data-phone-${dataID}`).text());
-            $("#changeEmail").val($(`#data-email-${dataID}`).text());
-            $("#changeAdress").val($(`#data-address-${dataID}`).text());
-            $("#changeVeID").val($(`#data-license-${dataID}`).text());
-            $("#changeBrand").val($(`#data-brand-${dataID}`).text());
-            $("#changeDate").val($(`#data-date-${dataID}`).text());
-
-            // close change form with x
-            $('#closeFormChange').on('click', function(){
-                $('#formChange').addClass('hidden');
-                $('#formChange').removeClass('show');
-                $('body').css('overflow', 'auto');
-                $('#dataTable').css({
-                    'opacity': '',
-                    'pointer-events': ''
-                });
-                $('#formChange input').val('');
-            });
-
-        };
-  
-  
+      // close change form with x
+      $("#closeFormChange").on("click", function () {
+        $("#formChange").addClass("hidden");
+        $("#formChange").removeClass("show");
+        $("body").css("overflow", "auto");
+        $("#dataTable").css({
+          opacity: "",
+          "pointer-events": "",
+        });
+        $("#formChange input").val("");
+      });
+    }
     function updateData(dataID, newData) {
       console.log("after sub ", test);
       var index = test.findIndex((item) => item.ID === dataID);
@@ -355,68 +353,68 @@ async function fetchData() {
 }
 
 fetchData();
-
-function handleForm2() {
-  // User nhập số đt vào ô, nhấn search => Gửi form về server => server gửi data.
-  let list = [];
-  $(document).ready(() => {
-    $("#main-screen-content-order2 form").submit(function (event) {
-      event.preventDefault();
-      $("#main-screen-content-order2 table").css("display", "table");
-      $.ajax({
-        url: "http://localhost:3000/list",
-        method: "GET",
-        dataType: "JSON",
-        success: function (data) {
-          printData(data);
-          list = data;
-        },
-        error: function (error) {
-          console.log(error);
-        },
-      });
-    });
-  });
-  // Print data
-  function printData(data) {
-    let html = "";
-    for (let i = 0; i < data.length; i++) {
-      html += `
-    <tr> 
-    <td> ${data[i]["ID"]}</td>
-    <td> ${data[i]["Vehicle license number"]}</td>
-    <td> ${data[i]["Vehicle brand"]}</td>
-    <td> ${data[i]["Name"]}</td>
-    <td> ${data[i]["Debt"]}</td>
-    </tr>
-    `;
-    }
-    // console.log(html);
-    $(document).ready(function () {
-      const body = $("table tbody#myTableBody");
-      body.html(html);
-    });
-  }
-
-  // Sort data
-  $("#main-screen-content-order2 th").on("click", function () {
-    const column = $(this).data("column");
-    const order = $(this).data("order");
-    const textArray = $(this).html().split(" ");
-    console.log(textArray[0]);
-    if (order !== undefined) {
-      if (order === "desc") {
-        list = list.sort((a, b) => (a[column] > b[column] ? 1 : -1));
-        $(this).data("order", "asc");
-        $(this).html(textArray[0] + " &#9650");
-      } else if (order === "asc") {
-        list = list.sort((a, b) => (a[column] < b[column] ? 1 : -1));
-        $(this).data("order", "desc");
-        $(this).html(textArray[0] + " &#9660");
-      }
-    }
-    printData(list);
-    console.log($(this).html());
-  });
-}
-handleForm2();
+//
+// function handleForm2() {
+//   // User nhập số đt vào ô, nhấn search => Gửi form về server => server gửi data.
+//   let list = [];
+//   $(document).ready(() => {
+//     $("#main-screen-content-order2 form").submit(function (event) {
+//       event.preventDefault();
+//       $("#main-screen-content-order2 table").css("display", "table");
+//       $.ajax({
+//         url: "http://localhost:3000/list",
+//         method: "GET",
+//         dataType: "JSON",
+//         success: function (data) {
+//           printData(data);
+//           list = data;
+//         },
+//         error: function (error) {
+//           console.log(error);
+//         },
+//       });
+//     });
+//   });
+//   // Print data
+//   function printData(data) {
+//     let html = "";
+//     for (let i = 0; i < data.length; i++) {
+//       html += `
+//     <tr>
+//     <td> ${data[i]["ID"]}</td>
+//     <td> ${data[i]["Vehicle license number"]}</td>
+//     <td> ${data[i]["Vehicle brand"]}</td>
+//     <td> ${data[i]["Name"]}</td>
+//     <td> ${data[i]["Debt"]}</td>
+//     </tr>
+//     `;
+//     }
+//     // console.log(html);
+//     $(document).ready(function () {
+//       const body = $("table tbody#myTableBody");
+//       body.html(html);
+//     });
+//   }
+//
+//   // Sort data
+//   $("#main-screen-content-order2 th").on("click", function () {
+//     const column = $(this).data("column");
+//     const order = $(this).data("order");
+//     const textArray = $(this).html().split(" ");
+//     console.log(textArray[0]);
+//     if (order !== undefined) {
+//       if (order === "desc") {
+//         list = list.sort((a, b) => (a[column] > b[column] ? 1 : -1));
+//         $(this).data("order", "asc");
+//         $(this).html(textArray[0] + " &#9650");
+//       } else if (order === "asc") {
+//         list = list.sort((a, b) => (a[column] < b[column] ? 1 : -1));
+//         $(this).data("order", "desc");
+//         $(this).html(textArray[0] + " &#9660");
+//       }
+//     }
+//     printData(list);
+//     console.log($(this).html());
+//   });
+// }
+// handleForm2();
