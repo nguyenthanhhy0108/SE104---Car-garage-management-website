3fde8f93eac56d98c13e110c6156063b0d0f1f18
huynhchinhan <cicilian@MacBook-Air-cua-huynhchinhan.local>
2024-06-13 11:45:27 +0700

    Xong

--- src/main/resources/static/js/new_vehicle.js
+++ src/main/resources/static/js/new_vehicle.js
@@ -56,37 +56,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 fetch_build();
 async function fetch_build() {
   try {
-    // Lấy ra số entry
-    let entries = parseInt($("#number-entry").val());
-
-    // Page hiển thị khi vừa vào trang là 1
-    let cur_page = 1;
-
     // Khoi tao mang chua toan bo du lieu
     let dataTable = await fetchData();
     // Mảng thật sự sử dụng
     let dataInUse = dataTable;
 
-    // Hiển thị thanh chuyển trang
-    setupPagination();
-
     //  Hiển thị bảng
-    displayTable(cur_page, entries, dataInUse);
-
-    // Khi người dùng nhấn chuyển trang
-    $("#pagination").on("click", "button", function () {
-      console.log("successj");
-      cur_page = $(this).html;
-      goToPage($(this).html());
-    });
-
-    // Nhận tín hiệu khi có sự thay đổi về số phần tử của một trang
-    $("#number-entry").on("change", function () {
-      entries = parseInt($("#number-entry").val());
-      cur_page = 1;
-      setupPagination();
-      displayTable(cur_page, entries, dataInUse);
-    });
+    displayTable(dataInUse);
 
     // Xử lý search
     $("#search-input").on("keyup", function () {
@@ -94,11 +70,10 @@ async function fetch_build() {
       console.log("Value", value);
       // get Table Data from database
       var filteredData = searchTable(value, dataTable);
+      // Sửa lại dataInUse
       dataInUse = filteredData;
-      // Sửa lại số trang
-      setupPagination();
-      cur_page = 1;
-      displayTable(cur_page, entries, dataInUse);
+      // Hiển thị bảng
+      displayTable(dataInUse);
     });
     function searchTable(value, data) {
       var filteredData = [];
@@ -113,59 +88,38 @@ async function fetch_build() {
       return filteredData;
     }
 
-    // Hàm chuyển trang
-    function goToPage(page) {
-      cur_page = page;
-      displayTable(cur_page, entries, dataInUse);
-    }
     // Hàm hiển thị bảng theo trang
-    function displayTable(page, entry, data) {
+    function displayTable(data) {
       const tableBody = $("#myTable");
       tableBody.empty();
 
-      const start = (page - 1) * entry;
-      const end = entry + start;
-      // console.log(start, end);
-      const paginatedData = data.slice(start, end);
-      for (let i = 0; i < paginatedData.length; i++) {
-        // console.log(paginatedData.length);
-        // console.log(paginatedData[i]);
-        let numOrder = paginatedData[i].cars.length;
+      for (let i = 0; i < data.length; i++) {
+        // Đếm số lượng xe
+        let numOrder = data[i].cars.length;
         // console.log(numOrder);
         if (numOrder != 0) {
-          let row = `<tr scope="row" class="data-row-${paginatedData[i].id}" data-id="${paginatedData[i].id}">
-          <td rowspan="${numOrder}" id="data-name-${paginatedData[i]["id"]}" >${paginatedData[i]["id"]}</td>
-          <td rowspan="${numOrder}" id="data-name-${paginatedData[i]["name"]}" >${paginatedData[i]["name"]}</td>
-          <td id="data-order-brand-${paginatedData[i].cars[0]["brand"]}" >${paginatedData[i].cars[0]["brand"]}</td>
-          <td id="data-order-license-${paginatedData[i].cars[0]["licenseNumber"]}" >${paginatedData[i].cars[0]["licenseNumber"]}</td>
-          <td id="data-order-debt-${paginatedData[i].cars[0]["debt"]}" >${paginatedData[i].cars[0]["debt"]}</td>
+          let row = `<tr scope="row" class="data-row-${data[i].id}" data-id="${data[i].id}">
+          <td rowspan="${numOrder}" id="data-name-${data[i]["id"]}" >${data[i]["id"]}</td>
+          <td rowspan="${numOrder}" id="data-name-${data[i]["name"]}" >${data[i]["name"]}</td>
+          <td id="data-order-brand-${data[i].cars[0]["brand"]}" >${data[i].cars[0]["brand"]}</td>
+          <td id="data-order-license-${data[i].cars[0]["licenseNumber"]}" >${data[i].cars[0]["licenseNumber"]}</td>
+          <td id="data-order-debt-${data[i].cars[0]["debt"]}" >${data[i].cars[0]["debt"]}</td>
           </tr>`;
 
           tableBody.append(row);
 
           for (let j = 1; j < numOrder; j++) {
             let subRow = `<tr>
-            <td id="data-order-brand-${paginatedData[i].cars[j]["brand"]}" >${paginatedData[i].cars[j]["brand"]}</td>
-            <td id="data-order-license-${paginatedData[i].cars[j]["licenseNumber"]}" >${paginatedData[i].cars[j]["licenseNumber"]}</td>
-            <td id="data-order-debt-${paginatedData[i].cars[j]["debt"]}" >${paginatedData[i].cars[j]["debt"]}</td>
+            <td id="data-order-brand-${data[i].cars[j]["brand"]}" >${data[i].cars[j]["brand"]}</td>
+            <td id="data-order-license-${data[i].cars[j]["licenseNumber"]}" >${data[i].cars[j]["licenseNumber"]}</td>
+            <td id="data-order-debt-${data[i].cars[j]["debt"]}" >${data[i].cars[j]["debt"]}</td>
             </tr>`;
             tableBody.append(subRow);
           }
         }
       }
     }
-    // Hàm hiển thị thanh chuyển trang
-    function setupPagination() {
-      const totalPage = Math.ceil(dataInUse.length / entries);
-      const pagination = $("#pagination");
 
-      pagination.empty();
-      122;
-
-      for (let i = 1; i <= totalPage; i++) {
-        pagination.append(`<button>${i}</button>`);
-      }
-    }
     // Hàm lấy data
     async function fetchData() {
       try {
